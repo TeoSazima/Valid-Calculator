2233bc8562cf822896a24f0c2950ea23b44a3df3
TeoSazima <teo.sazima@gmail.com>
2025-03-08 19:21:57 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-def lcm(x, y):
-    """
-        Find least common multiple of 2 positive integers.
-        :param x: int - first integer
-        :param y: int - second integer
-        :return: int - least common multiple
+
